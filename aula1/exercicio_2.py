1302a2d69f614c3541d403babf8a5a0dd4c977d1
Renan Gregório <145120433+RenanGreg@users.noreply.github.com>
2025-02-14 09:28:10 -0300

    Create exercicio_2.py

---
+++ aula1/exercicio_2.py
@@ -0,0 +1,12 @@
+def ola(): 
+    nome = input("insira o nome \n")
+    print(f"ola {nome}, Bem vindo ao curso \n")
+
+def idade(): 
+    ano_atual = int(input("insira o ano atual \n"))
+    ano_nasc= int(input("insira o ano de seu nascimento \n")) 
+    idade = ano_atual - ano_nasc 
+    print(f"idade no ano {ano_atual} é {idade}") 
+
+ola()
+idade()
