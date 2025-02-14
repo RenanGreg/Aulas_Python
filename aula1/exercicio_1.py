4ab78e971c268b8926e39ff31277c4f53a1cddb3
Renan Gregório <145120433+RenanGreg@users.noreply.github.com>
2025-02-14 09:27:32 -0300

    Create exercicio_1.py

---
+++ aula1/exercicio_1.py
@@ -0,0 +1,15 @@
+def soma(n1 , n2): 
+    return n1 + n2 
+
+def sub (n1 , n2): 
+    return n1 - n2 
+
+n1 = int(input("insira o valor de n1 \n")) 
+
+n2 = int(input("insira o valor de n2 \n")) 
+
+s = soma (n1 , n2) 
+sb = sub (n1 , n2) 
+
+print(f"soma {s} \subtração {sb} \n")  
+print("soma", s, "subtração" , sb)
